ce14447c628d8ab847013c35089623aef8723271
Kverde <Kverde@yandex.ru>
2018-01-03 12:43:59 +0500

    'fix'

--- source/setting.py
+++ source/setting.py
@@ -5,9 +5,9 @@ class Setting(BaseSettings):
     def __init__(self, settings_dir, settings_file_name = '.env'):
         super(Setting, self).__init__(settings_dir, settings_file_name)
 
-        self.telegram_token = self.loadSetting('TELEGRAM_TOKEN')
-        self.botan_token = self.loadSetting('BOTAN_TOKEN')
-        self.database_url = self.loadSetting('DATABASE_URL')
+        self.telegram_token = self.loadSetting('TELEGRAM_TOKEN', 'MAIN')
+        self.botan_token = self.loadSetting('BOTAN_TOKEN', 'MAIN')
+        self.database_url = self.loadSetting('DATABASE_URL', 'MAIN')
 
 
 
